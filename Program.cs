9f71bc80307a91ed09b9ae7487f493d8789bf5b2
Rinteh <103682829+Rinteh@users.noreply.github.com>
2023-04-24 17:30:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Timedate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2017, 6, 23);
+            Console.WriteLine("Some Date Formats : ");
+            Console.WriteLine("Date and Time: {0}", date);
+            Console.WriteLine(date.ToString("yyyy-MM-dd"));
+            Console.WriteLine(date.ToString("dd-MMM-yy"));
+            Console.WriteLine(date.ToString("M/d/yyyy"));
+            Console.WriteLine(date.ToString("M/d/yy"));
+            Console.WriteLine(date.ToString("MM/dd/yyyy"));
+            Console.WriteLine(date.ToString("MM/dd/yy"));
+            Console.WriteLine(date.ToString("yy/MM/dd"));
+            Console.Read();
+            
+        }
+    }
+}
