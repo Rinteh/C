0a9065fda4816d72c0d6331f761ba2a12405895d
Rinteh <103682829+Rinteh@users.noreply.github.com>
2023-04-24 17:54:23 +0200

    Add files via upload

---
+++ SwitchMarks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SwitchMark
+{
+    internal class SwitchMarks
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your grade: ");
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            switch (grade)
+            {
+                case int n when (n >= 74 && n <= 100):
+                    Console.WriteLine("Your grade is: A ");
+                    break;
+                case int n when (n > 59 && n < 75):
+                    Console.WriteLine("Your grade is: B");
+                    break;
+                case int n when (n > 49 && n < 60):
+                    Console.WriteLine("Your grade is: C");
+                    break;
+                case int n when (n > 39 && n < 50):
+                    Console.WriteLine("Your grade is: D");
+                    break;
+                case int n when (n < 40):
+                    Console.WriteLine("Your grade is: F");
+                    break;
+                default:
+                    Console.WriteLine("You have made incorrect input. ");
+                    break ;
+            }
+        }
+    }
+}
---
+++ TaxApps.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TaxApp
+{
+    internal class TaxApps
+    {
+        static void Main(string[] args)
+        { const Double cc = 0.06;
+            Console.WriteLine("Enter item price for tax to be calculated:\n");
+            Double Price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("*********************************************");
+            Double tx = Price * cc;
+
+            Double inc = tx + Price;
+                        Console.WriteLine("Tax is at 6%\n");
+            Console.WriteLine("*********************************************\n");
+            Console.WriteLine("The item price with tax included is: "+ inc);
+        }
+    }
+}
