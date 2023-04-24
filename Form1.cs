0c1d473bcd9e14d460e8752c2aed6e87607e7bd8
Rinteh <103682829+Rinteh@users.noreply.github.com>
2023-04-24 17:01:58 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String word, piglatin;
+            word = textBox1.Text;
+
+            piglatin = word.Substring(1,word.Length-1)+word.Substring(0,1)+"ay";
+            label2.Text = piglatin;
+        }
+    }
+}
