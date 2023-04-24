c1756450f7f77b0fa7f92534672d721a8d3f0618
Rinteh <103682829+Rinteh@users.noreply.github.com>
2023-04-24 18:14:32 +0200

    Add files via upload

---
+++ MonthIF.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace MonthEnum
+{
+    internal class MonthIF
+    {
+
+        static void Main(string[] args)
+        {
+
+            int MonNum;
+            Console.WriteLine("Enter a Number from 1 to 12: ");
+            Console.WriteLine("*********************************");
+            MonNum = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (MonNum == 1)
+            {
+                Console.WriteLine("January");
+            }
+            else if (MonNum == 2)
+            {
+                Console.WriteLine("February");
+            }
+            else if (MonNum == 3)
+            {
+                Console.WriteLine("March");
+
+            }
+        }
+    }
+}
